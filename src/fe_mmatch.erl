78a593eb535e130bd560b9a5bc2231574b3d6230
Torben Hoffmann <th@MacBook-Pro.local>
2015-04-01 11:36:04 +0200

    Relx works and changed fe_mmatch to use records only

--- src/fe_mmatch.erl
+++ src/fe_mmatch.erl
@@ -1,8 +1,10 @@
 -module(fe_mmatch).
 
--export([new/4,
+-export([new/3,
          tick/1,
          ticks/2]).
+
+-export([test/1]).
 
 -type minute() :: non_neg_integer().
 -type score() :: {non_neg_integer(), non_neg_integer()}.
@@ -15,24 +17,27 @@
 
 -type team_info() :: {string(), {float(), float()}}.
 
--type match_data() :: 'undefined' |
-                      #{lh => float(),
-                        la => float(),
-                        stoppage1 => 0..3,
-                        stoppage2 => 0..6}.
+-record(match_data,
+        {lh :: float(),
+         la :: float(),
+         stoppage1 :: 0..3,
+         stoppage2 :: 0..6}).
+
+-type match_data() :: 'undefined' | #match_data{}.
 
 -record(match,
-        {match_id :: pos_integer(),
-         home     :: team_info(),
+        {home     :: team_info(),
          away     :: team_info(),
          status   :: status(),
          match_data :: match_data()}).
 
-%-type match() :: #match{}.
+-type match() :: #match{}.
 
-new(MatchId, {_HomeName,{_HomeDef,_HomeAtt}}=Home, {_AwayName, {_AwayDef,_AwayAtt}}=Away, StartsIn) ->
-  #match{ match_id =MatchId, 
-          home = Home,
+-export_type([match/0]).
+
+-spec new(term(),term(), non_neg_integer()) -> match().
+new({_HomeName,{_HomeDef,_HomeAtt}}=Home, {_AwayName, {_AwayDef,_AwayAtt}}=Away, StartsIn) ->
+  #match{ home = Home,
           away = Away, 
           status = {starts_in, StartsIn},
           match_data = undefined}.
@@ -42,28 +47,30 @@ tick(#match{status={finished, _}}=M) ->
 tick(#match{status={starts_in, 0}}=M) ->
   {_, {HomeDef, HomeAtt}} = M#match.home,
   {_, {AwayDef, AwayAtt}} = M#match.away,
-  MatchData = #{ lh => (HomeAtt * AwayDef * 1.37) /90,
-                 la => (AwayAtt * HomeDef) /90,
-                 stoppage1 => stoppage(1),
-                 stoppage2 => stoppage(2) }, 
-  M#match{ status = {first_half, 0, {0,0}},
+  MatchData = #match_data{ lh = (HomeAtt * AwayDef * 1.37) /90,
+                 la = (AwayAtt * HomeDef) /90,
+                 stoppage1 = stoppage(1),
+                 stoppage2 = stoppage(2) }, 
+  M#match{ status = {first_half, 1, {0,0}},
            match_data = MatchData};
 tick(#match{status={starts_in, N}}=M) ->
   M#match{status={starts_in, N-1}};
 tick(#match{status={halftime, 0, Score}}=M) ->
-  M#match{status = {second_half, 0, Score}};
+  M#match{status = {second_half, 1, Score}};
+tick(#match{status={halftime, N, Score}}=M) ->
+  M#match{status={halftime, N-1, Score}};
 tick(#match{status={first_half, T, Score},
-            match_data=#{stoppage1 := S1}}=M) when T>45+S1 ->
+            match_data=#match_data{stoppage1 = S1}}=M) when T>45+S1 ->
   M#match{status={halftime, 15, Score}};
 tick(#match{status={first_half, T, {Gh, Ga}},
             match_data=MD}=M) ->
-  #{ lh := Lh, la := La} = MD,
+  #match_data{ lh = Lh, la = La} = MD,
   M#match{ status={first_half, T+1, {Gh+goal(Lh), Ga+goal(La)}} };
 tick(#match{status={second_half, T, Score},
-            match_data=#{stoppage2 := S2}}=M) when T>45+S2 ->
+            match_data=#match_data{stoppage2 = S2}}=M) when T>45+S2 ->
   M#match{ status = {finished, Score} };
 tick(#match{status={second_half, T, {Gh, Ga}}}=M) ->
-  #{ lh := Lh, la := La} = M#match.match_data,
+  #match_data{ lh = Lh, la = La} = M#match.match_data,
   M#match{ status={second_half, T+1, {Gh+goal(Lh), Ga+goal(La)}} }.
 
 
@@ -87,3 +94,6 @@ stoppage(1) ->
 stoppage(2) ->
   crypto:rand_uniform(1,3) + crypto:rand_uniform(0,4).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+test(1) ->
+  new({"ManU", {2.4, 0.34}}, {"ManC", {2.7, 0.4}}, 5).

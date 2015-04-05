c9cbcd928a6ecd20d07b303723261634f797841c
Torben Hoffmann <th@MacBook-Pro.local>
2015-04-05 20:38:54 +0200

    Randomised the random numbers.

--- src/fe_app.erl
+++ src/fe_app.erl
@@ -5,8 +5,13 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+  seed(),
   fe_match_id:init(),
   fe_sup:start_link().
 
 stop(_State) ->
   ok.
+
+seed() ->
+  {A, B, C} = now(),
+  random:need(A, B, C).

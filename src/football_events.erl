fae24f05e6469fca611650420fb65648a120c09a
Torben Hoffmann <th@MacBook-Pro.local>
2015-04-05 20:41:06 +0200

    Add match_info/1 and tick related functions to football_events

--- src/football_events.erl
+++ src/football_events.erl
@@ -2,16 +2,16 @@
 
 
 -export([new_match/0,
-         matches/0]).
+         matches/0,
+         match_info/1]).
 
 -export([subscribe/1,
          unsubscribe/1]).
 
-%% -export([start_ticks/0,
-%%          tick_interval/0,
-%%          set_tick_interval/1]).
-
-
+-export([start_ticks/0,
+         stop_ticks/0,
+         tick_interval/0,
+         set_tick_interval/1]).
 
 %% @doc Generates a new match with two random teams, to start in 10..60 minutes.
 new_match() ->
@@ -27,6 +27,7 @@ new_match() ->
                            {Away, AwayStrength},
                            StartsIn).
   
+%% @doc returns a list of match ids
 matches() ->
   Key = {fe_match, '$1'},
   GProcKey = {n, l, Key},
@@ -35,9 +36,31 @@ matches() ->
   Result = ['$1'],
   gproc:select([{MatchHead, Guard, Result}]).
 
+%% @doc returns a pair consisting of the pair of teams involed and the current
+%% status of the match.
+match_info(MatchId) ->
+  {fe_match:teams(MatchId), fe_match:status(MatchId)}.
+
+%% @doc subscribes the calling process to match_info events from the MatchId match.
+%% The info has the shape {match_info, match_id()}, status()}. 
 subscribe(MatchId) ->
   gproc:reg({p, l, {match_info, MatchId}}).
 
 unsubscribe(MatchId) ->
   gproc:unreg({p, l, {match_info, MatchId}}).
 
+
+%% @doc start the ticks that drives forward the matches.
+start_ticks() ->
+  fe_ticks:run().
+
+stop_ticks() ->
+  fe_ticks:stop().
+
+%% @doc get the time in milli seconds between the ticks.
+tick_interval() ->
+  fe_ticks:interval().
+
+%% @doc set the tick interval to T milli seconds.
+set_tick_interval(T) ->
+  fe_ticks:set_interval(T).
